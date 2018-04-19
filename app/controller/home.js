810d6f7a62fce3e70ac93a53892b0043003b773c
FFFfengfengfeng <1098865109@qq.com>
2018-04-19 17:46:21 +0800

    路由添加,样式reset

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,7 +7,41 @@ class HomeController extends Controller {
   //首页
   async index() {
     const ctx = this.ctx;
-    await ctx.render('index.ejs', {data: 'hello world!'});
+    let data = {
+      active: 'index',
+      title: 'FFF-首页'
+    }
+    await ctx.render('index.ejs', {data: data});
+  }
+
+  //html
+  async hyper() {
+    const ctx = this.ctx;
+    let data = {
+      active: 'hyper',
+      title: 'FFF-HTML'
+    }
+    await ctx.render('hyper.ejs', {data: data});
+  }
+
+  //CSS
+  async style() {
+    const ctx = this.ctx;
+    let data = {
+      active: 'style',
+      title: 'FFF-CSS'
+    }
+    await ctx.render('style.ejs', {data: data});
+  }
+
+  //ECMA
+  async ecma() {
+    const ctx = this.ctx;
+    let data = {
+      active: 'ecma',
+      title: 'FFF-JS'
+    }
+    await ctx.render('ecma.ejs', {data: data});
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/hyper', controller.home.hyper);
+  router.get('/style', controller.home.style);
+  router.get('/ecma', controller.home.ecma);
 };
